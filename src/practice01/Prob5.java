ae1db3d9d30861aa1d56bbcf4b082f9be60f70e9
최나현 <@February4th>
2017-01-05 13:42:44 +0900

    Prob5 edit

--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -4,25 +4,28 @@ public class Prob5 {
 
 	public static void main(String[] args) {
 
-		int num = 1;
-		int dnum = 0;
 		
-		while (num<100){
-			if (num <= 10){
-				if (num % 3 == 0){
-					System.out.println(num + " 짝");
-				}
-			}
+		for (int i=0; i<100; i++){
+			String snum = String.valueOf(i);
+			int length = snum.length();
+			int clapCount = 0;
+			
+			for (int j=0; j<length; j++){
+				char n = snum.charAt(j);
+				if (n=='3' || n=='6' || n=='9'){
+					clapCount++;
+						}
+					}
+			if (clapCount == 0){
+				continue;
+					}			
 			else {
-				dnum = num % 10;
-				if (dnum % 3 == 0){
-					System.out.println(num + " 짝");
-				}
-			}
-			num ++;
+				System.out.print(snum);
+				for (int k=0; k<clapCount; k++){
+					System.out.print("짝");
+					}
+				System.out.println();
+				}	
 		}
-
-		
-		
 	}
 }
